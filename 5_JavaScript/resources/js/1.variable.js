d01ec982bb51416fc7d927e0ffaee1815d1472b2
alwaysJOne <alwaysjone@naver.com>
2024-08-29 11:57:45 +0900

    .

---
+++ 5_JavaScript/resources/js/1.variable.js
@@ -0,0 +1,16 @@
+console.log("Hello World");
+
+/**
+ * js의 변수 선언
+ * 
+ * 1) var - 사용하지 않는다.
+ * 2) let 
+ * 3) const
+ */
+
+var name = "최지원";
+var age = 55;
+var height = 190.7;
+var isTrue = true;
+
+console.log(name + " " + age + " " + height + " " + isTrue) 
