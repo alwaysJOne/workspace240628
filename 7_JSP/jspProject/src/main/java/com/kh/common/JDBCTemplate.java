cbbf63404004f12c42df9c371f6dac8b887e96cb
alwaysJOne <alwaysjone@naver.com>
2024-09-12 14:50:03 +0900

    .

---
+++ 7_JSP/jspProject/src/main/java/com/kh/common/JDBCTemplate.java
@@ -0,0 +1,107 @@
+package com.kh.common;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class JDBCTemplate {
+	//모두 static메소드
+	//싱글톤 패턴 : 메모리영역에 단 한번만 올려두고 매번 재사용하는 개념
+	
+	//1. Connection객체 생성(DB접속) 후 해당 Connection객체 반환
+	public static Connection getConnection() {
+		
+		Connection conn = null;
+		Properties prop = new Properties();
+		
+		//읽어들이고자하는 classes폴더내의 driver.properties파일의 물리적인 경로 가져오기
+		String filePath = JDBCTemplate.class.getResource("/db/driver.properties").getPath();
+		//System.out.println(filePath);
+		
+		
+		try {
+			prop.load(new FileInputStream(filePath));
+			
+			//1) JDBC Driver등록
+			Class.forName(prop.getProperty("driver"));
+			
+			//2)Connection객체 생성
+			conn = DriverManager.getConnection(prop.getProperty("url"), prop.getProperty("username"), prop.getProperty("password"));
+			conn.setAutoCommit(false);//수동커밋 설정
+			System.out.println("성공");
+			
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return conn;
+	}
+	
+	//2) commit처리해주는 메소드(Connection객체를 전달 받아서)
+	public static void commit(Connection conn) {
+		try {
+			if (conn != null && !conn.isClosed()) {
+				conn.commit();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//3) rollback처리해주는 메소드(Connection객체를 전달 받아서)
+	public static void rollback(Connection conn) {
+		try {
+			if (conn != null && !conn.isClosed()) {
+				conn.rollback();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//JDBC용 객체들을 전달받아서 반납처리해주는 메소드
+	//4) Statement관련 객체를 전달받아서 반납시켜주는 메소드
+	public static void close(Statement stmt) {
+		try {
+			if(stmt != null && !stmt.isClosed()) {
+				stmt.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//5) Connection 객체를 전달받아서 반납시켜주는 메소드
+	public static void close(Connection conn) {
+		try {
+			if(conn != null && !conn.isClosed()) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//6) ResultSet 객체를 전달받아서 반납시켜주는 메소드
+	public static void close(ResultSet rset) {
+		try {
+			if(rset != null && !rset.isClosed()) {
+				rset.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
--- 7_JSP/jspProject/src/main/webapp/index.jsp
+++ 7_JSP/jspProject/src/main/webapp/index.jsp
@@ -1,3 +1,4 @@
+<%@page import="com.kh.common.JDBCTemplate"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <!DOCTYPE html>
@@ -8,5 +9,31 @@
 </head>
 <body>
 
+	<!-- 
+		*회원서비스
+					| C(Insert) | R(Select) | U(Update) | D(Delete) |
+		==============================================================================
+		   로그인		|			| 	o		|			|			|
+		   회원가입    |	o		| 			|			|			|
+		  마이페이지	|			| 	o		|			|			|
+		   정보수정	|			| 			|	o		|			|
+		   회원탈퇴	|			| 			|	o		|			|
+		[아이디중복체크] |			| 	o		|			|			|
+		
+		*공지사항
+		공지사항 리스트 조회(R) / 상세조회(R)
+		공지사항 작성(C) / 공지사항 수정(U) / 공지사항 삭제(U)
+		
+		*일반게시판
+		게시글 리스트 조회(R) - 페이징처리 / 상세조회(R)
+		게시글 작성(C) / 게시글 수정(U) / 게시글 삭제(U)
+		[댓글리스트 조회(R) / 댓글작성(C)]
+		
+		*사진게시판
+		게시판 리스트 조회(R) - 썸네일형식 / 상세조회(R)
+		게시글 작성(C) - 첨부파일업로드
+ 	-->
+ 	
+ 	<% JDBCTemplate.getConnection(); %>
 </body>
 </html>
